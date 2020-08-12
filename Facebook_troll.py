8013bbe27f22e731023510dd37bb609bc9dfc494
ShaharEli <65499638+ShaharEli@users.noreply.github.com>
2020-08-13 00:16:25 +0300

    Update Facebook_troll.py

--- Facebook_troll.py
+++ Facebook_troll.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.keys import Keys
 from password import password1,user1 #password and username for facebook
-#from password import lyrics
 #import random
 import os
 lk = os.path.join(os.getcwd(), "chromedriver",)
@@ -26,10 +25,8 @@ sendd.click()
 sleep(2)
 messg=driver.find_element_by_xpath('/html/body/div[1]/div[7]/div[1]/div/div/div[4]/div/div[1]/div/div/div/div/div/div[1]/div/div[2]/div[4]/div/div/div/span/div/div/div[2]/div/div/div/div')
 counter=1
-#lst=["יזין","יא אלרגי למנגו","יא אלרגי ליין","אתה חכם כמו שאתה נמוך","גולאש של בוקר","יאצבע שפיץ","אינשאללה שמי שיינשק אותך היום בלילה יהיה לו זין בגודל של פיסטוק..","cold thank you","תגיד אני מכיר אותך בכלל?","יא גנב קניות מטיב טעם","יחרא באופי","ילא נראה טוב","מי ייתן ולא תתחתן",lyrics,"ינקניק","מי שקורא את זה מת להיות שחר","ימדפיס דילדואים בתלת מימד","יאוכל שווארמה לארוחת בוקר","🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕"]
 while True:
     lol="bulbul number: "+str(counter)+" 8============}"
-    #lol=random.choice(lst)
     try:
         messg = driver.find_element_by_xpath(
             '/html/body/div[1]/div[7]/div[1]/div/div/div[4]/div/div[1]/div/div/div/div/div/div[1]/div/div[2]/div[4]/div/div/div/span/div/div/div[2]/div/div/div/div')
